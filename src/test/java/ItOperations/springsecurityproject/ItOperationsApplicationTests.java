62943230e252f1109df2b71236f3910b55c31b87
kimchaeeun3447 <rlacodms3447@naver.com>
2024-01-01 20:27:18 +0900

    setting: spring boot initializer로 프로젝트 추가 #1
    - 개발환경 : spring boot 3.2.1, java 21, gradle 선택
    - dependency : spring web, security, jpa, lombok, h2

---
+++ src/test/java/ItOperations/springsecurityproject/ItOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package ItOperations.springsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
