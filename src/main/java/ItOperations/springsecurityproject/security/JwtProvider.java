cf442732f9cc55978d421503e6f8b43e1b625c87
kimchaeeun3447 <rlacodms3447@naver.com>
2024-01-02 02:27:48 +0900

    feat: JWT 생성 및 검증을 위한 JwtProvider 작성 #6

---
+++ src/main/java/ItOperations/springsecurityproject/security/JwtProvider.java
@@ -0,0 +1,98 @@
+package ItOperations.springsecurityproject.security;
+
+import ItOperations.springsecurityproject.member.domain.Authority;
+import ItOperations.springsecurityproject.security.JpaUserDetailsService;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+/* JWT 생성 및 검증 */
+
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret.key}")
+    private String salt;
+
+    private Key secretKey;
+
+    // JWT 만료 시간 - 1시간
+    private final long exp = 1000L * 60;
+
+    private final JpaUserDetailsService jpaUserDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // JWT 생성
+    public String createToken(String accountId, List<Authority> roles) {
+        Claims claims = Jwts.claims().setSubject(accountId);
+        claims.put("roles", roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + exp))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Spring Security 인증과정에서 권한 확인을 위한 권한 정보 획득
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = jpaUserDetailsService.loadUserByUsername(this.getAccount(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // JWT에서 회원의 accountId 획득
+    public String getAccount(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject();
+        } catch (ExpiredJwtException e) { // 만료된 토큰일 경우
+            e.printStackTrace();
+
+            return e.getClaims().getSubject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // Authorization Header로 받은 JWT 추출
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
+    }
+
+    // JWT 검증
+    public boolean validateToken(String token) {
+        try {
+            // Bearer 검증
+            if (!token.substring(0, "BEARER ".length()).equalsIgnoreCase("BEARER ")) {
+                return false;
+            } else {
+                token = token.split(" ")[1].trim();
+            }
+            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+
+            // JWT 만료되었으면 false, 아니면 true 반환
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
