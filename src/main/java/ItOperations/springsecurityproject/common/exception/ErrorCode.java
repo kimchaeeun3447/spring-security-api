22cf1d6787985a3c2837e820afa849209986af27
kimchaeeun3447 <rlacodms3447@naver.com>
2024-01-03 01:20:54 +0900

    refactor: TokenService의 토큰 검증 로직 에러 핸들링 추가  #12

--- src/main/java/ItOperations/springsecurityproject/common/exception/ErrorCode.java
+++ src/main/java/ItOperations/springsecurityproject/common/exception/ErrorCode.java
@@ -14,7 +14,9 @@ public enum ErrorCode {
 
     // Security, JWT
     UNAUTHORIZED_MEMBER(401, "인증되지 않은 사용자입니다."),
-    Forbidden_MEMBER(403, "권한이 없는 사용자입니다."),
+    FORBIDDEN_MEMBER(403, "권한이 없는 사용자입니다."),
+    INVALID_REFRESH_TOKEN(403, "토큰이 유효하지 않습니다. 재로그인이 필요합니다."),
+    NOT_FOUND_TOKEN(404, "토큰이 존재하지 않습니다. 재로그인이 필요합니다."),
 
     // Member
     INPUT_INVALID(400, "입력한 정보를 다시 확인해주세요."),
